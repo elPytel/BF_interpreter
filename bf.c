ef7df30b86030e94883f5bb7525b31b321f5f102
elPytel <jaroslav.korner1@gmail.com>
2021-03-18 01:18:38 +0100

    v 0.0.4 dynamic memory

--- bf.c
+++ bf.c
@@ -1,24 +1,69 @@
+# By Pytel
+"""
+Based on code by:
+@maxcountryman
+@himehowareu
+@D0tty
+"""
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-// initialize the tape with 30,000 zeroes
-unsigned char tape[30000] = {0};
+char* add_cell(char* tape, size_t *tape_size) {
+    char* tmp = (char*)realloc(tape, (++*tape_size) * sizeof(char));
 
-// set the pointer to point at the left-most cell of the tape
-unsigned char* ptr = tape;
+    if (tmp == NULL) {
+        printf("FATAL: Could not reallocate tape memory!");
+        exit(1);
+    }
+
+    if (!tape[*tape_size-1]) {
+        tape[*tape_size-1] = 0;
+    }
+
+    return tmp;
+}
+
+char* remove_cell(char* tape, size_t *tape_size, char* ptr) {
+    char tail = tape[*tape_size-1];
+
+    // if the cell is zeroed and not pointed to currently
+    if (tail == 0 && ptr != &tape[*tape_size-1] && *tape_size > 0) {
+        char* tmp = (char*)realloc(tape, (--*tape_size) * sizeof(char));
+        if (tmp == NULL) {
+            printf("FATAL: Could not reallocate tape memory!");
+            exit(1);
+        }
+        return tmp;
+    }
+    return tape;
+}
 
 
 void interpret(char* input) {
+	size_t tape_size = 1;
+	char* tape;
+	char* ptr;
+	long int index = 0;
+
 	char current_char;
 	size_t i;
 	size_t loop;
 
+	tape = (char*)calloc(tape_size, sizeof(char));
+	ptr = tape;
+
 	for (i = 0; input[i] != 0; i++) {
 		current_char = input[i];
 		if (current_char == '>') {
+			++index;
+			if (index >= tape_size) {
+				tape = add_cell(tape, &tape_size);
+			}
 			++ptr;
 		} else if (current_char == '<') {
+			--index;
 			--ptr;
 		} else if (current_char == '+') {
 			++*ptr;
@@ -51,9 +96,12 @@ void interpret(char* input) {
 				}
 			}
 		} else {			// random chars and comments
-			continue
+			continue;
 		}
+		// garbage collect
+		tape = remove_cell(tape, &tape_size, ptr);
 	}
+	free(tape);
 }
 
 
@@ -85,4 +133,8 @@ int main(int argc, char *argv[]) {
   free(file_buf);
   return 0;
 }
+
+"""
+END
+"""
 
